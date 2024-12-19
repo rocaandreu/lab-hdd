a89945e6b7a301e2038b90b0252674411f668380
ROCA MONTSERRAT <andreu.roca.montserrat@c5c4.upc.es>
2024-12-19 17:38:04 +0100

    [P7] finished

--- lab7/lab7_2/src/lpf_AC.cpp
+++ lab7/lab7_2/src/lpf_AC.cpp
@@ -9,7 +9,6 @@
 #include "lpf_AC.h" 
 #include "stdio.h"
 
-
 #pragma hls_design top // To set the Catapult top design block
 
 void lpf_ac (const X_TYPE i_sample, COEFF_TYPE b[], Y_TYPE &y)                 
@@ -30,6 +29,12 @@ y = filter_out;
 
 void lpf(const X_TYPE i_sample, COEFF_TYPE b[], Y_TYPE &y) 
   {
+  static Y_TYPE peaks[AVERAGED_PEAKS];
+  static ac_fixed<14,6,true,AC_RND,AC_SAT_SYM> peak_avg = 0;
+  static Y_TYPE peak;
+  static ac_fixed<10,10,true> counter;
+  static ac_fixed<12,2,false> gain = 1;
+
   // previous input history, remember across calls
   static X_TYPE x[TAP_COUNT];
   SHIFT_LOOP: for (int n=TAP_COUNT-1; n>0; n--) {
@@ -42,29 +47,52 @@ void lpf(const X_TYPE i_sample, COEFF_TYPE b[], Y_TYPE &y)
     sum += x[n] * b[n];
   }
   // round & saturate according to Y_TYPE
+
+  sum *= gain;
+
+  SUM_TYPE sum_abs = 0;
+  if (sum<0) {
+      sum_abs = -sum;
+  }
+  else {
+      sum_abs = sum;
+  }
   
-  if (sum<0){
-  	sum = -sum;
-  }  
+  if (counter.to_int() == peak_det_samples) {
+    // Store found peak and calculate average
+    counter  = 0;
+    peak_avg = 0;
+
+    std::cout << "NEW PEAK: " << peak.to_double() << std::endl;
+   
+    for (int i = AVERAGED_PEAKS-1; i > 0; i--) {
+    	peaks[i] = peaks[i-1];
+    	peak_avg += peaks[i];
+	std::cout << "PEAK_LIST: " << peaks[i].to_double() << std::endl;
+        std::cout << "PEAKS_SUM: " << peak_avg.to_double() << std::endl;
+    }
+    peaks[0] = peak;
+    peak_avg += peak;
+    std::cout << "PEAKS_SUM: " << peak_avg.to_double() << std::endl;
+    peak_avg /= AVERAGED_PEAKS;
+    peak = 0;
+    
+    // Calculate new gain
+    if (peak_avg.to_double() > x_high.to_double()) {
+    	// Reduce gain
+    	gain -= gain_delta;
+    }
+    else if (peak_avg.to_double() < x_low.to_double()) {	
+    	// Increase gain
+    	gain += gain_delta;
+    }
+
+    std::cout << "GAIN = " << gain.to_double() << std::endl;
+    std::cout << "PEAK AVG = " << peak_avg.to_double() << std::endl << std::endl;
+  } 
+
+  if (sum_abs > peak) peak = sum_abs;
+  counter++;
 
   y = sum;
 }
-
-
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
--- lab7/lab7_2/src/lpf_AC.h
+++ lab7/lab7_2/src/lpf_AC.h
@@ -9,16 +9,17 @@
 #include <ac_fixed.h>
 #include <ac_int.h>
 
-const unsigned FILTER_ORDER = 157; //120;        // filter order
+#define AVERAGED_PEAKS 6
+
+const unsigned FILTER_ORDER = 158; //120;        // filter order
 const unsigned TAP_COUNT = FILTER_ORDER+1;  // there are N+1 coefficients for an N-order filter
 
 // Number of samples to detect one peak: Half period of the lower filter frequency
 // Tlow/(2Ts) = fs/(2flow) = 10 kHz/600 Hz = ~17. Take 32 for simpler division
 const unsigned SAMPLES_PEAK_DETECT = 20; // 32;
-const unsigned PK_AVG_CNT = 8;// 32; // Number of averaged peaks
+//const unsigned PK_AVG_CNT = 8;// 32; // Number of averaged peaks
 
-const unsigned averaged_peaks = 8;
-const unsigned peak_det_samples = 50;	//10000 samples 
+const unsigned peak_det_samples = 45;	//10000 samples
 
 typedef ac_fixed<10,2,true>   X_TYPE;
 typedef ac_fixed<12,2,true> COEFF_TYPE;
@@ -28,15 +29,14 @@ typedef ac_fixed<12,4,true,AC_RND,AC_SAT_SYM> Y_TYPE;
 typedef X_TYPE::rt_T<COEFF_TYPE>::mult           PROD_TYPE;
 typedef PROD_TYPE::rt_unary::set<TAP_COUNT>::sum SUM_TYPE;
 
-SUM_TYPE peak_max = 0;
-X_TYPE counter = 0;
+const COEFF_TYPE gain_delta = 0.03;
 
 // Comparison constants
 const Y_TYPE neg1 = -1;
 const Y_TYPE x_max = 2;
 const Y_TYPE x_ref = x_max/2;
-const Y_TYPE x_high = x_ref + x_ref/64;
-const Y_TYPE x_low = x_ref - x_ref/64; 
+const Y_TYPE x_high = x_ref + x_ref/16;
+const Y_TYPE x_low = x_ref - x_ref/16; 
 const Y_TYPE x_lock = x_ref/8;
 const Y_TYPE x_inc_dec = x_ref/16;
 
